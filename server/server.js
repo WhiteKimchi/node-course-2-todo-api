5a099c89e2c7ca012e904185745fe1ff20c676bb
unknown <sean71574@gmail.com>
2017-12-27 22:43:49 -0700

    Add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ app.post('/todos', (req, res) => {
     });
 });
 
+// GET /todos/
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
@@ -50,6 +51,23 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send(todo);
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(3000, () => {
     console.log(`Started on port ${port}`);
