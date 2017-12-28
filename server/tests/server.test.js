73d97e05035297bb3daed572a4aba12214127b33
unknown <sean71574@gmail.com>
2017-12-27 22:55:29 -0700

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -94,7 +94,46 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 
-    it('should return 404 for non-object ids', (done) => {
+    it('should return 404 if object id is invalid', (done) => {
+        request(app)
+            .get('/todos/123')
+            .expect(404)
+            .end(done);
+    });
+});
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var newHexId = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${newHexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(newHexId);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                Todo.findById(newHexId).then((todo) => {
+                    expect(todo).toNotExist();
+                    done();
+                }).catch((e) => done(e));
+
+            });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var newHexId = new ObjectID().toHexString();
+
+        request(app)
+            .get(`/todos/${newHexId}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 if object id is invalid', (done) => {
         request(app)
             .get('/todos/123')
             .expect(404)
